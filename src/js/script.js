4f00b2bc1ec724f49667768565edb3f47004f282
Kristina <kgritsenko.2017@gmail.com>
2025-01-29 17:04:11 +0200

    feat: add header and hero section

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,9 @@
+'use strict';
+
 const body = document.querySelector('body');
+
+const modal = document.querySelectorAll('.faq-accordion__content');
+
+modal.addEventListener('click', function () {
+    modal.classList.remove('hidden')
+}) 
